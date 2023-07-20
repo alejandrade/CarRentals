d74effe6f51ee7b5a9a8fda4e260f66dfc794979
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-07-20 14:03:05 -0400

    Initial commit

---
+++ src/main/java/com/techisgood/carrentals/CarRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.techisgood.carrentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techisgood/carrentals/CarRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techisgood.carrentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
