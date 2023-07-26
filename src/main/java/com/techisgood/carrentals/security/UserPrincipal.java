2bcdc385ec3a4be5918426a48d55284e85e78e19
superjustin5000 <jfletch1787@gmail.com>
2023-07-26 10:31:07 -0500

    import fix

--- src/main/java/com/techisgood/carrentals/security/UserPrincipal.java
+++ src/main/java/com/techisgood/carrentals/security/UserPrincipal.java
@@ -1,15 +1,16 @@
 package com.techisgood.carrentals.security;
 
-import com.techisgood.carrentals.model.DbUser;
-import lombok.RequiredArgsConstructor;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.techisgood.carrentals.model.DbUser;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class UserPrincipal implements UserDetails {
