41f2fa3bf57472d9b60607fe4c43372c74d3e7c6
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-07-30 15:59:38 -0400

    fix exception code response

--- src/main/java/com/techisgood/carrentals/exception/InvalidPhoneNumberException.java
+++ src/main/java/com/techisgood/carrentals/exception/InvalidPhoneNumberException.java
@@ -1,5 +1,9 @@
 package com.techisgood.carrentals.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidPhoneNumberException extends IllegalArgumentException {
     public InvalidPhoneNumberException(String message) {
         super(message);
--- src/main/java/com/techisgood/carrentals/exception/RemoteServiceException.java
+++ src/main/java/com/techisgood/carrentals/exception/RemoteServiceException.java
@@ -1,5 +1,9 @@
 package com.techisgood.carrentals.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class RemoteServiceException extends Exception {
 	
 	//NOTE(justin): add new services here as we start implementing them in code.
