d14588a02f07e8bd18170bc4f8c5e916c7e7b25d
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-08-22 19:11:25 -0400

    removing cache for now

--- src/main/java/com/techisgood/carrentals/global/CacheConfig.java
+++ src/main/java/com/techisgood/carrentals/global/CacheConfig.java
@@ -7,15 +7,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-
 @Configuration
 @RequiredArgsConstructor
 @EnableCaching
 public class CacheConfig implements CachingConfigurer {
-
-    private final RedisConnectionFactory redisConnectionFactory;
 
     @Bean
     @Override
