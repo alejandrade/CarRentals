cbbb9d6d182e149936b93ecf751c709705512602
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-09-05 15:25:26 -0400

    fixing location drop down

--- src/main/java/com/techisgood/carrentals/payments/RemotePaymentsProperties.java
+++ src/main/java/com/techisgood/carrentals/payments/RemotePaymentsProperties.java
@@ -7,6 +7,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
@@ -30,11 +31,22 @@ public class RemotePaymentsProperties {
 	private final ObjectMapper objectMapper;
 
 	@PostConstruct
+	@Profile("prod")
 	public void init() throws JsonProcessingException {
 		GetSecretValueRequest apiSecretsArc = GetSecretValueRequest.builder().secretId("API_SECRETS_ARC").build();
 		GetSecretValueResponse secretValue = secretsManagerClient.getSecretValue(apiSecretsArc);
 		JsonNode jsonNode = objectMapper.readValue(secretValue.secretString(), JsonNode.class);
 		secretKey = jsonNode.get("STRIPE_SECRET_KEY").asText();
 		webhookSecret = jsonNode.get("STRIPE_WEBHOOK_SECRET").asText();
+	}
+
+	@PostConstruct
+	@Profile("dev")
+	public void initForDev() throws JsonProcessingException {
+		GetSecretValueRequest apiSecretsArc = GetSecretValueRequest.builder().secretId("DEV_API_SECRETS_ARC").build();
+		GetSecretValueResponse secretValue = secretsManagerClient.getSecretValue(apiSecretsArc);
+		JsonNode jsonNode = objectMapper.readValue(secretValue.secretString(), JsonNode.class);
+		secretKey = jsonNode.get("STRIPE_SECRET_KEY").asText();
+		webhookSecret = "dev";
 	}
 }
