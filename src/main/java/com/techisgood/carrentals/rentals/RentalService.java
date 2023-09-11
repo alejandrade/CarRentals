e7d1b8ba0cc0a0bb176eeee340d655c17061f907
Alejandro Andrade <alejandrodanielandrade@gmail.com>
2023-09-11 15:00:55 -0400

    moving insurance

--- src/main/java/com/techisgood/carrentals/rentals/RentalService.java
+++ src/main/java/com/techisgood/carrentals/rentals/RentalService.java
@@ -69,7 +69,6 @@ public class RentalService {
 		byId.setVersion(rentalActionDto.getVersion());
 		byId.setCleaningFee(rentalActionDto.getCleaningFee());
 		byId.setDamagedFee(rentalActionDto.getDamagedFee());
-		byId.setInsuranceFee(rentalActionDto.getInsuranceFee());
 		byId.setGasFee(rentalActionDto.getGasFee());
 		return RentalDto.from(rentalRepository.save(byId));
 	}
